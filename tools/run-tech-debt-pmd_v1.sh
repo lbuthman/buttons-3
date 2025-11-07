4f23419354d1be5106f6895c1ec05798256a380f
lukebuthman <lbuthman@gmail.com>
2025-11-07 05:52:59 -0600

    init

--- tools/run-tech-debt-pmd_v1.sh
+++ tools/run-tech-debt-pmd_v1.sh
@@ -4,3 +4,5 @@ sfdx scanner:run \
   --target "force-app/main/default/classes/$1.cls" \
   --outfile "scans/$1.json" \
   --pmdconfig "scans/level-0-tech-debt-ruleset.xml"
+
+#sfdx scanner:run --format json --target "force-app/main/default/classes/$1.cls" --outfile "scans/$1.json" --pmdconfig "scans/level-0-tech-debt-ruleset.xml"
